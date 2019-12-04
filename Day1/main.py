0b7c881285a85bc8e0341c6921e01e18c6db84c7
Tim Langen <tim.langen@driessen.nl>
2019-12-04 15:55:16 +0100

    Day 1, Part 1 and 2

---
+++ Day1/main.py
@@ -0,0 +1,43 @@
+import math
+
+
+def mathCalc(input):
+    return (math.floor(input / 3) - 2)
+
+
+def part1(input):
+    solution = 0
+    for x in input:
+        output = mathCalc(x)
+        solution += output
+    print(f"Part 1: {solution}")
+
+
+def calculateMass(input, total=0):
+    output = mathCalc(input)
+    total += output
+    if mathCalc(output) <= 0:
+        return total
+    else:
+        total += calculateMass(output)
+    return total
+
+
+def part2(input):
+    solution = 0
+
+    for x in input:
+        output = calculateMass(int(x))
+        solution += output
+    print(f"Part 2: {solution}")
+
+
+if __name__ == '__main__':
+    input = []
+
+    f = open('in/input.txt', 'r')
+    for x in f:
+        if x is not '':
+            input.append(int(x))
+    part1(input)
+    part2(input)
